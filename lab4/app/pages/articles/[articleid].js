a3ba1bff4a3b8b6be07df7457c618ac89fe213fb
Antoine-Versio <antoine.bertrand@edu.ece.fr>
2022-10-18 13:33:02 +0200

    feat: display articles informations, create a serveur-side web page

---
+++ lab4/app/pages/articles/[articleid].js
@@ -0,0 +1,50 @@
+import Header from '../../comps/header'
+import Footer from '../../comps/footer'
+import Menu from '../../comps/menu'
+import styles from '../../styles/Home.module.css'
+import content from '../../content/articles.js'
+import { useEffect} from 'react';
+//import Article from '../../content/articles.js'
+import { useRouter} from 'next/router'
+
+export default function DisplayArticles({dataExport}) {
+  const router = useRouter();
+  const {articleid} = router.query;
+
+return dataExport.map(function(item){
+
+    if (item.id === articleid) {
+      console.log(item.title)
+      console.log(item.content)
+      console.log(item.date)
+      console.log(item.author)
+      return(
+        <div>
+           <ul>
+              <li>
+              title : {item.title}
+              </li>
+              <li>
+                content : {item.content}
+              </li>
+              <li>
+                date : {item.date}
+              </li>
+              <li>
+                author : {item.author}
+              </li>
+          </ul>
+        </div>
+      );
+    }
+  });
+
+
+}
+
+export async function getServerSideProps(){
+  const url = 'http://localhost:3000/api/articlesblog';
+  const res = await fetch(url)
+  const dataExport = await res.json()
+  return {props : {dataExport}};
+}
