7d4ef48f2729230524496441d1e25db12d1e8dda
AlexiaBesnard <58394970+AlexiaBesnard@users.noreply.github.com>
2022-12-16 19:54:33 +0100

    feat: project base code

---
+++ project/app/pages/index.js
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../components/Layout.js'
+import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+import Account from '../components/Account'
+
+export default function Home() {
+
+  const session = useSession()
+  const supabase = useSupabaseClient()
+
+  return (
+    <Layout>
+      <Head>
+        <title>WebTech</title>
+        <meta name="description" content="Web technologies blogging application" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className='wt-title'>
+        Welcome to <a href="https://www.adaltas.com">web technologies!</a>
+      </h1>
+      <ul>
+        <li>
+          <Link href="/articles">
+            View our articles
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            About us
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            Contact us
+          </Link>
+        </li>
+      </ul>
+    </Layout>
+  )
+}
